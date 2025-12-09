3a8e05290e79e2828f10632c86fc06922ef98779
Maciej Sawka <maciejsawka@gmail.com>
2025-12-09 22:15:54 +0100

    Add unit tests and fix bugs uncovered by them
    In an effort to replace bin/testbin with proper unit tests,
    start writing some test cases in lib.rs, for the main code.
    They actaully uncovered two bugs already, one minor one major.
    
    Fix minor bug in which .clear() did not update self.length
    
    Fix major bug which caused overwriting inserts to drop both
    subtrees of overwritten node. In addition to that, replace code
    with more optimal mem::replace() on the value rather than
    allocation of a whole new Node struct.
    
    TODO: tests of iterators, more unit tests in lib, better coverage
    (currently the more complicated cases of remove() are not covered
    by tests).

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,10 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use std::cmp::{Ord, Ordering};
+use std::{
+    cmp::{Ord, Ordering},
+    mem,
+};
 
 mod iter;
 use iter::BSTMapIter;
@@ -64,7 +67,8 @@ impl<K: Ord, V> BSTMap<K, V> {
     }
 
     pub fn clear(&mut self) {
-        self.head = None
+        self.head = None;
+        self.length = 0;
     }
 
     pub fn insert(&mut self, key: K, value: V) -> Option<V> {
@@ -75,20 +79,19 @@ impl<K: Ord, V> BSTMap<K, V> {
             current_node = match current_node.as_ref().unwrap().key.cmp(&key) {
                 Ordering::Less => &mut current_node.as_mut().unwrap().right,
                 Ordering::Greater => &mut current_node.as_mut().unwrap().left,
-                Ordering::Equal => break,
+                Ordering::Equal => {
+                    // If we find node with equal key, means the value already exists - replace and early return
+                    // safe unwrap - current_node is Some
+                    let dest = &mut current_node.as_mut().unwrap().as_mut().value;
+                    return Some(mem::replace(dest, value));
+                }
             }
         }
 
-        let old_value = current_node.take();
+        // If we left the loop means value does not exist
         *current_node = Some(Box::new(Node::new(key, value)));
-
-        match old_value {
-            None => {
-                self.length += 1; // If old vlaue is None, means length increased
-                None
-            }
-            Some(node) => Some(node.value),
-        }
+        self.length += 1;
+        None
     }
 
     pub fn contains(&self, key: K) -> bool {
@@ -238,5 +241,148 @@ impl<K: Ord, V> BSTMap<K, V> {
 
     pub fn iter(&self) -> BSTMapIter<'_, K, V> {
         BSTMapIter::new(self)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::BSTMap;
+
+    #[test]
+    fn new_map_is_empty() {
+        let bst = BSTMap::<u32, String>::new();
+
+        assert_eq!(bst.len(), 0);
+        assert!(bst.is_empty());
+    }
+
+    #[test]
+    fn insertion_changes_length_but_duplicates_do_not() {
+        let mut bst = BSTMap::<u32, String>::new();
+
+        const SERIES_OF_INSERTIONS: [(u32, &str); 6] = [
+            (12, "hello"),
+            (35, "bye"),
+            (999, "test"),
+            (0, "test2"),
+            (1200, "high number"),
+            (36, "utf8 string ąąąąą"),
+        ];
+        const SERIES_OF_DUPLICATES: [(u32, &str); 6] = [
+            (12, "hello1"),
+            (35, "bye2"),
+            (999, "test3"),
+            (0, "test24"),
+            (1200, "high number5"),
+            (36, "utf8 string ąąąąą6"),
+        ];
+
+        for (k, v) in &SERIES_OF_INSERTIONS {
+            bst.insert(*k, v.to_string());
+        }
+
+        for (k, v) in &SERIES_OF_DUPLICATES {
+            bst.insert(*k, v.to_string());
+        }
+
+        // Duplicates should not change length!
+        assert_eq!(bst.len(), SERIES_OF_INSERTIONS.len());
+        assert!(!bst.is_empty());
+    }
+
+    #[test]
+    fn removal_changes_length() {
+        let mut bst = BSTMap::<u32, String>::new();
+
+        const KEY: u32 = 1;
+        const VALUE: &str = "test";
+        bst.insert(KEY, VALUE.to_string());
+
+        bst.remove(KEY);
+
+        assert_eq!(bst.len(), 0);
+        assert!(bst.is_empty());
+    }
+
+    #[test]
+    fn nonexistent_key_returns_none() {
+        let mut bst = BSTMap::<u32, String>::new();
+
+        const KEY: u32 = 999;
+
+        assert!(!bst.contains(KEY));
+        assert!(bst.get(KEY).is_none());
+        assert!(bst.get_mut(KEY).is_none());
+    }
+
+    #[test]
+    fn existent_key_returns_some() {
+        let mut bst = BSTMap::<u32, String>::new();
+
+        const KEY: u32 = 999;
+        const VALUE: &str = "something";
+        bst.insert(KEY, VALUE.to_string());
+
+        assert!(bst.contains(KEY));
+        assert!(bst.get(KEY).is_some());
+        assert!(bst.get_mut(KEY).is_some());
+        assert_eq!(*bst.get(KEY).unwrap(), VALUE.to_string());
+        assert_eq!(*bst.get_mut(KEY).unwrap(), VALUE.to_string());
+    }
+
+    #[test]
+    fn insertion_overwrites_existing_key() {
+        let mut bst = BSTMap::<u32, String>::new();
+
+        const KEY: u32 = 999;
+        const ORIGINAL_VALUE: &str = "hallo";
+        const NEW_VALUE: &str = "bye";
+        let mut return_val = bst.insert(KEY, ORIGINAL_VALUE.to_string());
+
+        assert!(return_val.is_none());
+
+        // overwrite
+        return_val = bst.insert(KEY, NEW_VALUE.to_string());
+
+        assert!(return_val.is_some());
+        assert_eq!(return_val.unwrap(), ORIGINAL_VALUE.to_string());
+    }
+
+    #[test]
+    fn removal_returns_none_on_nonexistent_key() {
+        let mut bst = BSTMap::<u32, String>::new();
+
+        const KEY: u32 = 1;
+        let return_val = bst.remove(KEY);
+
+        assert!(return_val.is_none());
+    }
+
+    #[test]
+    fn removal_returns_some_on_existent_key() {
+        let mut bst = BSTMap::<u32, String>::new();
+
+        const KEY: u32 = 1;
+        const VALUE: &str = "hello";
+        bst.insert(KEY, VALUE.to_string());
+
+        let return_val = bst.remove(KEY);
+
+        assert!(return_val.is_some());
+        assert_eq!(return_val.unwrap(), VALUE.to_string());
+    }
+
+    #[test]
+    fn clear_sets_length_to_zero() {
+        let mut bst = BSTMap::<u32, String>::new();
+
+        const KEY: u32 = 1;
+        const VALUE: &str = "hi";
+        bst.insert(KEY, VALUE.to_string());
+
+        bst.clear();
+
+        assert_eq!(bst.len(), 0);
+        assert!(bst.is_empty());
     }
 }
